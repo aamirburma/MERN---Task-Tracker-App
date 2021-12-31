acb9f5b0de8d2834ad53b39d77e1e6b5f3ae536b
Aamirsohel Burma <burmaaamir@gmail.com>
2022-01-01 01:52:57 +0530

    Add Login, Regisration, Task-insert

--- mern-task-tracker-app/src/App.js
+++ mern-task-tracker-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Home from './components/home/home'
+import Login from './components/login/login';
+import Register from './components/register/register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios';
 
 function App() {
+
+  /** Store logined user details in useState */
+  const [user, setUser] = useState({});
+  const [taskDetails, setTaskDetails] = useState({})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {
+              user && user.email ? <Home loginUser={user} setUser={setUser} /> : <Login setUser={setUser} />
+            }
+          </Route>
+          <Route exact path="/login">
+            <Login setUser={setUser} />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
